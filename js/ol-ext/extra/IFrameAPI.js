6e27320786c369e3cf3ad363f92ce53b640c7b26
Sameh AbuRadi <Sameh.AbuRadi@CODE.Berlin>
2021-09-08 17:25:35 +0300

    norm'ed code

--- js/ol-ext/extra/IFrameAPI.js
+++ js/ol-ext/extra/IFrameAPI.js
@@ -3,11 +3,11 @@ if (window.ol && !ol.ext) {
   ol.ext = {};
 }
 /** IFrame API create an api and wait the target ready
- * @constructor 
+ * @constructor
  * @param {string} targetOrigin, default '*'
  */
-ol.ext.IFrameAPI = function(targetOrigin) {
-  this.targetOrigin = targetOrigin || '*';
+ol.ext.IFrameAPI = function (targetOrigin) {
+  this.targetOrigin = targetOrigin || "*";
   this.id = -1;
   this.properties = {};
   // Setter api
@@ -15,76 +15,86 @@ ol.ext.IFrameAPI = function(targetOrigin) {
   // Listener api
   this.listener = {};
   // Wait for target ready
-  window.addEventListener('message', function(e) {
-    if (e.data.listener) {
-      if (this.listener[e.data.listener]) {
-        this.listener[e.data.listener].call(e.data.data);
-      }
-    } else {
-      switch (e.data.api) {
-        case 'ready': {
-          this.id = e.data.id;
-          window.parent.postMessage(e.data, this.targetOrigin);
-          break;
+  window.addEventListener(
+    "message",
+    function (e) {
+      if (e.data.listener) {
+        if (this.listener[e.data.listener]) {
+          this.listener[e.data.listener].call(e.data.data);
         }
-        case 'getAPI': {
-          e.data.data = Object.keys(this.setter);
-          e.data.id = this.id;
-          window.parent.postMessage(e.data, this.targetOrigin);
-          break;
-        }
-        default: {
-          if (this.setter[e.data.api]) {
-            var data = this.setter[e.data.api].call(this, e.data.data);
-            if (data !== undefined) {
-              e.data.data = data;
-              e.data.id = this.id;
-              window.parent.postMessage(e.data, this.targetOrigin);
-            }
+      } else {
+        switch (e.data.api) {
+          case "ready": {
+            this.id = e.data.id;
+            window.parent.postMessage(e.data, this.targetOrigin);
+            break;
           }
-          break;
+          case "getAPI": {
+            e.data.data = Object.keys(this.setter);
+            e.data.id = this.id;
+            window.parent.postMessage(e.data, this.targetOrigin);
+            break;
+          }
+          default: {
+            if (this.setter[e.data.api]) {
+              var data = this.setter[e.data.api].call(this, e.data.data);
+              if (data !== undefined) {
+                e.data.data = data;
+                e.data.id = this.id;
+                window.parent.postMessage(e.data, this.targetOrigin);
+              }
+            }
+            break;
+          }
         }
       }
-    }
-  }.bind(this), false);
+    }.bind(this),
+    false
+  );
   // ready
-  window.parent.postMessage({
-    api: 'ready'
-  }, this.targetOrigin);
-}
+  window.parent.postMessage(
+    {
+      api: "ready",
+    },
+    this.targetOrigin
+  );
+};
 /** Add properties
  * @param {string} key
  * @param {*} value
  */
-ol.ext.IFrameAPI.prototype.set = function(key, value) {
+ol.ext.IFrameAPI.prototype.set = function (key, value) {
   this.properties[key] = value;
 };
 /** Get properties
  * @param {string} key
  * @return {*}
  */
-ol.ext.IFrameAPI.prototype.get = function(key) {
+ol.ext.IFrameAPI.prototype.get = function (key) {
   return this.properties[key];
 };
-/** 
+/**
  * @typedef {Object} TemplateAPI
  * @property {string} name api name
  * @property {function} function if return a Transferable it will be send to the iFrame
  */
-/** Add functions to the API 
+/** Add functions to the API
  * @param {Array<TemplateAPI>} list of functions to add to the api
  */
-ol.ext.IFrameAPI.prototype.setAPI = function(api) {
+ol.ext.IFrameAPI.prototype.setAPI = function (api) {
   if (/\bready\b|\bgetAPI\b|\bon\b|\bun\b|\baddInput\b/.test(api)) {
-    console.error('Bad API key: '+api);
+    console.error("Bad API key: " + api);
   } else {
     for (var k in api) {
       this.setter[k] = api[k];
-      window.parent.postMessage({
-        api: 'getAPI',
-        id: this.id,
-        data: [k]
-      }, this.targetOrigin);
+      window.parent.postMessage(
+        {
+          api: "getAPI",
+          id: this.id,
+          data: [k],
+        },
+        this.targetOrigin
+      );
     }
   }
 };
@@ -92,13 +102,16 @@ ol.ext.IFrameAPI.prototype.setAPI = function(api) {
  * @param {string} name api name
  * @param {Transferable } data object to transfer to the iframe
  */
-ol.ext.IFrameAPI.prototype.postMessage = function(name, data) {
-  window.parent.postMessage({ 
-    api: name,
-    id: this.id,
-    data: data
-  }, this.targetOrigin);
+ol.ext.IFrameAPI.prototype.postMessage = function (name, data) {
+  window.parent.postMessage(
+    {
+      api: name,
+      id: this.id,
+      data: data,
+    },
+    this.targetOrigin
+  );
 };
-ol.ext.IFrameAPI.prototype.addListener = function(name, listener) {
+ol.ext.IFrameAPI.prototype.addListener = function (name, listener) {
   this.listener[name] = listener;
 };
